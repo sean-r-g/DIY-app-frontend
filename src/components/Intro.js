40ae26b354e07665581287899624a0df370dd3a7
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-06-20 13:11:17 -0500

    tweaked intro wording

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,7 +9,7 @@ const Intro = (props) => {
         <>
             <div className='intro-div'>
                 <h3>Welcome to HandyHome -- you're one stop shop for all home fixes, improvements, and more!</h3>
-                <h4>Feel free to browse, or go ahead and create and account to add, edit, or remove guides</h4>
+                <h4>Feel free to browse -- or go ahead and create and account to add, edit, or remove guides</h4>
             </div>
         </>
     )
