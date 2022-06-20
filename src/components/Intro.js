4790b1a77a6f16743e11e0c137ee45604a17c532
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-06-20 12:58:53 -0500

    added a bit of mobile styling and put Edit behind login

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,8 +8,8 @@ const Intro = (props) => {
     return (
         <>
             <div className='intro-div'>
-                <h3>Intro....</h3>
-                <h4>More words.....</h4>
+                <h3>Welcome to HandyHome -- you're one stop shop for all home fixes, improvements, and more!</h3>
+                <h4>Feel free to browse, or go ahead and create and account to add, edit, or remove guides</h4>
             </div>
         </>
     )
